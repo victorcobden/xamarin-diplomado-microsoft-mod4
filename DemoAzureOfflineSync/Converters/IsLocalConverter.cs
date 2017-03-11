55161eeca7af2bd4bbe64d3a0f0e52e1243a9002
victorcobden <vhcobden@gmail.com>
2017-03-11 13:29:08 -0500

    Init

---
+++ DemoAzureOfflineSync/Converters/IsLocalConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace DemoAzureOfflineSync.Converters
+{
+    public class IsLocalConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool val = (bool)value;
+
+            ImageSource synced = Device.OnPlatform(
+            iOS: ImageSource.FromFile("synced.png"),
+            Android: ImageSource.FromFile("synced.png"),
+            WinPhone: ImageSource.FromFile("synced.png"));
+
+            ImageSource notsynced = Device.OnPlatform(
+            iOS: ImageSource.FromFile("notsynced.png"),
+            Android: ImageSource.FromFile("notsynced.png"),
+            WinPhone: ImageSource.FromFile("notsynced.png"));
+
+            return (val) ? notsynced : synced;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DemoAzureOfflineSync/Models/Contact.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoAzureOfflineSync.Models
+{
+    [DataTable("Contacts")]
+    public class Contact
+    {
+        [JsonProperty("Id")]
+        public string Id { get; set; }
+        [JsonProperty("Name")]
+        public string Name { get; set; }
+        [Version]
+        public string Version { get; set; }
+        [JsonProperty("IsLocal")]
+        public bool IsLocal { get; set; }
+        [JsonProperty("Phone")]
+        public string Phone { get; set; }
+    }
+}
